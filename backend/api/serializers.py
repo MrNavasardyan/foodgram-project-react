8d870cd03644b69ab836a53ac7fdbb9aa776f030
ngrachik <ngrachik2011@gmail.com>
2023-06-01 20:13:22 +0300

    Changes to be committed: 	modified:   api/serializers.py

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -15,29 +15,46 @@ from rest_framework import status
 
 
 class CustomUserCreateSerializer(UserSerializer):
-    email = serializers.EmailField(
-        max_length=254, allow_blank=False, validators=[validate_email]
-    )
-    username = serializers.CharField(
-        max_length=150,
-        allow_blank=False,
-        validators=[
-            RegexValidator(
-                regex=r'^[\w.@+-]+\Z', message='Введите корректный username'
-            ),
-            UniqueValidator(queryset=User.objects.all()),
-        ],
-    )
-    first_name = serializers.CharField(max_length=150, required=True)
-    last_name = serializers.CharField(max_length=150, required=True)
-    password = serializers.CharField(max_length=150, required=True)
+    is_subscribed = serializers.SerializerMethodField()
 
     class Meta:
         model = User
-        fields = ('email', 'username', 'first_name', 'last_name', 'password')
+        fields = ('email', 'id', 'username', 'first_name',
+                  'last_name', 'password', 'is_subscribed')
+        extra_kwargs = {'password': {'write_only': True},
+                        'is_subscribed': {'read_only': True}}
+
+    def get_is_subscribed(self, obj):
+        user = self.context.get('request').user
+        if not user.is_anonymous:
+            return Follow.objects.filter(user=user, author=obj).exists()
+        return False
 
     def create(self, validated_data):
         return User.objects.create_user(**validated_data)
+    # email = serializers.EmailField(
+    #     max_length=254, allow_blank=False, validators=[validate_email]
+    # )
+    # username = serializers.CharField(
+    #     max_length=150,
+    #     allow_blank=False,
+    #     validators=[
+    #         RegexValidator(
+    #             regex=r'^[\w.@+-]+\Z', message='Введите корректный username'
+    #         ),
+    #         UniqueValidator(queryset=User.objects.all()),
+    #     ],
+    # )
+    # first_name = serializers.CharField(max_length=150, required=True)
+    # last_name = serializers.CharField(max_length=150, required=True)
+    # password = serializers.CharField(max_length=150, required=True)
+
+    # class Meta:
+    #     model = User
+    #     fields = ('email', 'username', 'first_name', 'last_name', 'password')
+
+    # def create(self, validated_data):
+    #     return User.objects.create_user(**validated_data)
 
 
 class CustomTokenCreateSerializer(TokenCreateSerializer):
@@ -304,6 +321,7 @@ class RecipeCreateSerializer(serializers.ModelSerializer):
 
 
 class RecipeMiniSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Recipe
         fields = ('id', 'name', 'cooking_time', 'image',)
