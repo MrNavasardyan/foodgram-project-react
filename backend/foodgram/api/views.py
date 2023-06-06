f4cedcabd38c289ab27236562e6f15e401917966
ngrachik <ngrachik2011@gmail.com>
2023-06-06 22:25:05 +0300

    Changes to be committed: 	modified:   api/views.py

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -168,7 +168,7 @@ class RecipeViewSet(viewsets.ModelViewSet):
         recipe = get_object_or_404(Recipe, id=kwargs['pk'])
 
         if request.method == 'POST':
-            serializer = FavoriteSerializer(recipe, data=request.data,
+            serializer = CustomUserSerializer(recipe, data=request.data,
                                           context={"request": request})
             serializer.is_valid(raise_exception=True)
             if not Favorite.objects.filter(user=request.user,
