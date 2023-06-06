804f2d7458f2da46c6b9d14ae5828b92b0463ac8
ngrachik <ngrachik2011@gmail.com>
2023-06-06 21:03:31 +0300

    Changes to be committed: 	modified:   api/views.py

--- backend/foodgram/api/views.py
+++ backend/foodgram/api/views.py
@@ -17,7 +17,6 @@ from rest_framework.permissions import (
 from django.core.handlers.wsgi import WSGIRequest
 from rest_framework.response import Response
 from users.models import CustomUser
-from .utils import shopping_cart
 from datetime import datetime as dt
 from django.http.response import HttpResponse
 from recipes.models import (
@@ -28,6 +27,7 @@ from recipes.models import (
     RecipeIngredient,
     Tag,
 )
+from utils import shopping_cart
 from users.models import CustomUser, Follow
 
 from .filters import IngredientLookupFilter, RecipeFilter
@@ -222,43 +222,16 @@ class RecipeViewSet(viewsets.ModelViewSet):
             return self.delete_favorite_or_shopping_cart(ShoppingCart, user, recipe)
         return Response(status=status.HTTP_400_BAD_REQUEST)
 
-    # @action(methods=('get',), detail=False)
-    # def download_shopping_cart(self, request: WSGIRequest) -> Response:
-    #     """Загружает файл *.txt со списком покупок.
-    #     Считает сумму ингредиентов в рецептах выбранных для покупки.
-    #     Возвращает текстовый файл со списком ингредиентов.
-    #     Вызов метода через url:  */recipes/download_shopping_cart/.
-    #     Args:
-    #         request (WSGIRequest): Объект запроса..
-    #     Returns:
-    #         Responce: Ответ с текстовым файлом.
-    #     """
-    #     user = self.request.user
-    #     if not user.carts.exists():
-    #         return Response(status=HTTP_400_BAD_REQUEST)
-
-    #     filename = f'{user.username}_shopping_list.txt'
-    #     shopping_list = [
-    #         f'Список покупок для:\n\n{user.first_name}\n'
-    #         f'{dt.now()}\n'
-    #     ]
-
-    #     ingredients = Ingredient.objects.filter(
-    #         recipe__recipe__in_carts__user=user
-    #     ).values(
-    #         'name',
-    #         measurement=F('measurement_unit')
-    #     ).annotate(amount=Sum('recipe__amount'))
-
-    #     for ing in ingredients:
-    #         shopping_list.append(
-    #             f'{ing["name"]}: {ing["amount"]} {ing["measurement"]}'
-    #         )
-
-    #     shopping_list.append('\nПосчитано в Foodgram')
-    #     shopping_list = '\n'.join(shopping_list)
-    #     response = HttpResponse(
-    #         shopping_list, content_type='text.txt; charset=utf-8'
-    #     )
-    #     response['Content-Disposition'] = f'attachment; filename={filename}'
-    #     return response
+    @action(detail=False,
+            methods=['get'],
+            permission_classes=[IsAuthenticated])
+    def download_shopping_cart(self, request):
+        """
+        Скачать список покупок для выбранных рецептов,
+        данные суммируются.
+        """
+        author = CustomUser.objects.get(id=self.request.user.pk)
+        if author.shopping_cart.exists():
+            return shopping_cart(self, request, author)
+        return Response('Список покупок пуст.',
+                        status=status.HTTP_404_NOT_FOUND)
