b1ada69109b9400920839208b0d1a35a9214ce44
ngrachik <ngrachik2011@gmail.com>
2023-06-06 19:18:26 +0300

    Changes to be committed: 	new file:   api/__init__.py 	new file:   api/apps.py 	new file:   api/filters.py 	new file:   api/migrations/__init__.py 	new file:   api/mixins.py 	new file:   api/pagination.py 	new file:   api/permissions.py 	new file:   api/serializers.py 	new file:   api/urls.py 	new file:   api/views.py 	new file:   foodgram/__init__.py 	new file:   foodgram/asgi.py 	new file:   foodgram/settings.py 	new file:   foodgram/urls.py 	new file:   foodgram/wsgi.py 	new file:   manage.py 	new file:   recipes/__init__.py 	new file:   recipes/admin.py 	new file:   recipes/apps.py 	new file:   recipes/migrations/0001_initial.py 	new file:   recipes/migrations/0002_initial.py 	new file:   recipes/migrations/__init__.py 	new file:   recipes/models.py 	new file:   requirements.txt 	new file:   users/__init__.py 	new file:   users/admin.py 	new file:   users/apps.py 	new file:   users/migrations/0001_initial.py 	new file:   users/migrations/__init__.py 	new file:   users/models.py

---
+++ backend/foodgram/api/views.py
@@ -0,0 +1,276 @@
+import os
+from django.conf import settings
+from django.db.models import Sum
+from django.http.response import HttpResponse
+from django_filters.rest_framework import DjangoFilterBackend
+from djoser.views import UserViewSet
+from reportlab.pdfbase import pdfmetrics, ttfonts
+from reportlab.pdfgen import canvas
+from rest_framework import status, viewsets
+from rest_framework.decorators import action
+from rest_framework.generics import get_object_or_404
+from rest_framework.permissions import (
+    AllowAny,
+    IsAuthenticated,
+    IsAuthenticatedOrReadOnly,
+)
+from rest_framework.response import Response
+
+from recipes.models import (
+    ShoppingCart,
+    Favorite,
+    Ingredient,
+    Recipe,
+    RecipeIngredient,
+    Tag,
+)
+from users.models import CustomUser, Follow
+
+from .filters import IngredientLookupFilter, RecipeFilter
+from .pagination import CustomPageNumberPagination
+from .permissions import AdminOrReadOnly, AuthorOrReadOnly
+from .serializers import (
+    CartSerializer,
+    CustomUserSerializer,
+    FavoriteSerializer,
+    FollowSerializer,
+    IngredientSerializer,
+    RecipeCreateSerializer,
+    RecipeListSerializer,
+    TagSerializer,
+)
+
+
+class CustomUserViewSet(UserViewSet):
+    """Представление для обработки запросов к ресурсу пользователей."""
+
+    queryset = CustomUser.objects.all()
+    serializer_class = CustomUserSerializer
+    permission_classes = (AllowAny,)
+
+    @action(
+        methods=('GET',),
+        detail=False,
+        permission_classes=(IsAuthenticated,),
+        url_path='me',
+    )
+    def get_current_user(self, request):
+        user = request.user
+        serializer = CustomUserSerializer(
+            user,
+            context={'request': request},
+        )
+        if user.is_anonymous:
+            return Response(
+                {'users': 'Учетные данные не были предоставлены.'},
+                status=status.HTTP_401_UNAUTHORIZED,
+            )
+        return Response(
+            serializer.data,
+            status=status.HTTP_200_OK,
+        )
+
+    @action(
+        methods=('GET',),
+        detail=False,
+        permission_classes=(IsAuthenticated,),
+        url_path='subscriptions',
+    )
+    def subscriptions(self, request):
+        user = request.user
+        queryset = Follow.objects.filter(user=user)
+        pages = self.paginate_queryset(queryset)
+        serializer = FollowSerializer(
+            pages,
+            many=True,
+            context={'request': request},
+        )
+        return self.get_paginated_response(serializer.data)
+
+    @action(
+        methods=('POST', 'DELETE'),
+        detail=True,
+        permission_classes=(IsAuthenticated,),
+    )
+    def subscribe(self, request, id=None):
+        user = request.user
+        author = get_object_or_404(CustomUser, id=id)
+        if request.method == 'POST':
+            if user == author:
+                return Response(
+                    {
+                        'subscribe': (
+                            'Нельзя просто так взять и подписаться на себя.'
+                        )
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            elif Follow.objects.filter(user=user, author=author).exists():
+                return Response(
+                    {'subscribe': 'Подписка на этого автора уже активна.'},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            follow = Follow.objects.create(user=user, author=author)
+            serializer = FollowSerializer(follow, context={'request': request})
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        follow = Follow.objects.filter(user=user, author=author)
+        if follow.exists():
+            follow.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return Response(
+            {'subscribe': 'Ранее вы уже отписались от этого автора.'},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
+
+
+class TagViewSet(viewsets.ReadOnlyModelViewSet):
+    """Представление для обработки запросов к ресурсу тегов."""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+    permission_classes = (AdminOrReadOnly,)
+    pagination_class = None
+
+
+class IngredientViewSet(viewsets.ReadOnlyModelViewSet):
+    """Представление для обработки запросов к ресурсу ингредиентов."""
+
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+    pagination_class = None
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = IngredientLookupFilter
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """Представление для обработки запросов к ресурсу рецептов."""
+
+    queryset = Recipe.objects.all()
+    permission_classes = (AuthorOrReadOnly,)
+    pagination_class = CustomPageNumberPagination
+    filter_backends = (DjangoFilterBackend,)
+    filterset_class = RecipeFilter
+
+    @staticmethod
+    def post_favorite_or_shopping_cart(model, user, recipe):
+        model_create, create = model.objects.get_or_create(
+            user=user, recipe=recipe
+        )
+        if create:
+            if str(model) == 'Favorite':
+                serializer = FavoriteSerializer()
+            else:
+                serializer = CartSerializer()
+            return Response(
+                serializer.to_representation(instance=model_create),
+                status=status.HTTP_201_CREATED,
+            )
+
+    @staticmethod
+    def delete_favorite_or_shopping_cart(model, user, recipe):
+        model.objects.filter(user=user, recipe=recipe).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def get_serializer_class(self):
+        if self.action in ('retrieve', 'list'):
+            return RecipeListSerializer
+        return RecipeCreateSerializer
+
+    @action(
+        methods=('POST', 'DELETE'),
+        detail=True,
+        filter_backends=DjangoFilterBackend,
+        filterset_class=RecipeFilter,
+    )
+    def favorite(self, request, pk):
+        user = request.user
+        recipe = get_object_or_404(Recipe, pk=pk)
+        if request.method == 'POST':
+            if Favorite.objects.filter(user=user, recipe=recipe).exists():
+                return Response(
+                    {'favorite': 'Рецепт уже добавлен в избранное.'},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            return self.post_favorite_or_shopping_cart(Favorite, user, recipe)
+        elif request.method == 'DELETE':
+            return self.delete_favorite_or_shopping_cart(
+                Favorite, user, recipe
+            )
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    @action(
+        methods=('POST', 'DELETE'),
+        detail=True,
+        permission_classes=(IsAuthenticated,),
+    )
+    def shopping_cart(self, request, pk=None):
+        user = request.user
+        recipe = get_object_or_404(Recipe, pk=pk)
+        if request.method == 'POST':
+            if ShoppingCart.objects.filter(user=user, recipe=recipe).exists():
+                return Response(
+                    {'shopping_cart': 'Рецепт уже добавлен в список покупок.'},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+            return self.post_favorite_or_shopping_cart(ShoppingCart, user, recipe)
+        elif request.method == 'DELETE':
+            return self.delete_favorite_or_shopping_cart(ShoppingCart, user, recipe)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    @action(
+        methods=('GET',),
+        detail=False,
+        permission_classes=(IsAuthenticated,),
+    )
+    def download_shopping_cart(self, request):
+        user = request.user
+        ingredients = (
+            RecipeIngredient.objects.filter(recipe__shoppingcart__user=user)
+            .values(
+                'ingredient__name',
+                'ingredient__measurement_unit',
+            )
+            .annotate(ingredient_sum=Sum('amount'))
+            .values_list(
+                'ingredient__name',
+                'ingredient_sum',
+                'ingredient__measurement_unit',
+            )
+        )
+        response_list = {}
+        for item in ingredients:
+            name = item[0]
+            if name not in response_list:
+                response_list[name] = {
+                    'amount': item[1],
+                    'measurement_unit': item[2],
+                }
+            else:
+                response_list[name]['amount'] += item[2]
+        pdfmetrics.registerFont(
+            ttfonts.TTFont(
+                'AGHelveticaCyr',
+                os.path.join(settings.BASE_DIR, 'fonts', 'AGHelveticaCyr.ttf'),
+                'UTF-8',
+            )
+        )
+        response = HttpResponse(content_type='application/pdf')
+        shopping_cart_list = canvas.Canvas(response)
+        shopping_cart_list.setFont('AGHelveticaCyr', size=24)
+        shopping_cart_list.drawString(200, 770, 'Cписок покупок')
+        shopping_cart_list.setFont('AGHelveticaCyr', size=14)
+        height = 700
+        for list_number, (name, data) in enumerate(response_list.items(), 1):
+            shopping_cart_list.drawString(
+                80,
+                height,
+                (
+                    f'{list_number}. {name} - {data["amount"]} '
+                    f'{data["measurement_unit"]}'
+                ),
+            )
+            height -= 20
+        shopping_cart_list.showPage()
+        shopping_cart_list.save()
+        return response
---
+++ backend/foodgram/foodgram/settings.py
@@ -0,0 +1,176 @@
+import os
+from pathlib import Path
+
+
+ENV_PATH = Path('../infra') / '.env'
+
+DEFAULT_LABEL_VALUE = '-пусто-'
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+CSRF_TRUSTED_ORIGINS = [
+        'http://*',
+        'https://*',
+        'http://localhost:8000',
+        'https://localhost:8000',
+        'http://localhost:8000',
+        'http://localhost:8080',
+        'http://backend:8000'
+        ]
+
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+    'backend',
+    'localhost',
+    '*'
+]
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-jta$4)v*a!k((5y*@l!oa3for22u)-a7=vhyei7l6sotos%hkr"
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    'rest_framework',
+    'rest_framework.authtoken',
+    'django_filters',
+    'djoser',
+    'recipes',
+    'users',
+    'api',
+]
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "foodgram.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "foodgram.wsgi.application"
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/4.2/topics/i18n/
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/4.2/howto/static-files/
+
+STATIC_URL = "static/"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'apistatic')
+
+MEDIA_URL = '/apimedia/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'apimedia')
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.CustomUser'
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 6,
+}
+
+DJOSER = {
+    'LOGIN_FIELD': 'email',
+    'SEND_ACTIVATION_EMAIL': False,
+    'HIDE_USERS': False,
+    'SERIALIZERS': {
+        'user_create': 'api.serializers.CustomUserCreateSerializer',
+        'user': 'api.serializers.CustomUserSerializer',
+        'current_user': 'api.serializers.CustomUserSerializer',
+    },
+    'PERMISSIONS': {
+        'user': ('djoser.permissions.CurrentUserOrAdminOrReadOnly',),
+        'user_list': ('rest_framework.permissions.AllowAny',),
+    },
+}
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
