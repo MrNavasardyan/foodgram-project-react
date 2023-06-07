ade9c7b1cb6b283b206996e85d24c1ba0153342c
ngrachik <ngrachik2011@gmail.com>
2023-06-07 16:36:59 +0300

    Changes to be committed: 	modified:   api/serializers.py

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -335,22 +335,6 @@ class FavoriteSerializer(serializers.ModelSerializer):
     cooking_time = serializers.CharField(
         read_only=True, source='recipe.cooking_time'
     )
-    def validate(self, obj):
-        user = self.context.get('request').user
-        recipe = self.context.get('request').recipe
-        if user == recipe.author:
-            raise serializers.ValidationError(
-                {
-                    'recipes': (
-                        'Подписываться на свой рецепт запрещено'
-                    )
-                }
-            )
-        if Favorite.objects.filter(user=user, recipe=recipe).exists():
-            raise serializers.ValidationError(
-                {'recipes': 'Рецепт уже добавлен в избранное.'}
-            )
-        return Favorite.objects.filter(user=user, recipe=recipe)
     # def validate(self, data):
     #     user = data['user']
     #     recipe = data['recipe']
@@ -376,6 +360,13 @@ class FavoriteSerializer(serializers.ModelSerializer):
             'image',
             'cooking_time',
         )
+        validators = [
+            UniqueTogetherValidator(
+                queryset=Favorite.objects.all(),
+                fields=('user', 'recipe'),
+                message='Рецепт уже добавлен в избранное'
+            )
+        ]
 
 
 class CartSerializer(serializers.ModelSerializer):
