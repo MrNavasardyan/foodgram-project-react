ec13cd3ff0290888fc075a06f902a09aaa4f48c6
ngrachik <ngrachik2011@gmail.com>
2023-06-07 17:33:31 +0300

    Changes to be committed: 	modified:   api/filters.py

--- backend/foodgram/api/filters.py
+++ backend/foodgram/api/filters.py
@@ -21,11 +21,11 @@ class RecipeFilter(filters.FilterSet):
     tags = filters.AllValuesMultipleFilter(field_name='tags__slug')
 
     def favorite_filter(self, queryset, name, value):
-        recipes = Recipe.objects.filter(favorites__user=self.request.user)
+        recipes = Recipe.objects.filter(favorites__author=self.request.user)
         return recipes
 
     def cart_filter(self, queryset, name, value):
-        recipes = Recipe.objects.filter(cart__user=self.request.user)
+        recipes = Recipe.objects.filter(cart__author=self.request.user)
         return recipes
 
     class Meta:
