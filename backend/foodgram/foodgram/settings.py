b98076e58e3101dddb47341c337d7cfa8e35707e
ngrachik <ngrachik2011@gmail.com>
2023-06-08 20:16:50 +0300

    Changes to be committed: 	modified:   foodgram/settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -20,7 +20,7 @@ CSRF_TRUSTED_ORIGINS = [
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'backend',
+    'web',
     'localhost',
     '*'
 ]
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = [
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
