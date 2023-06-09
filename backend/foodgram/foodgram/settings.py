e988d2561d221df10cfd3c23839dc92438276ac2
ngrachik <ngrachik2011@gmail.com>
2023-06-09 15:15:05 +0300

    Changes to be committed: 	modified:   ../backend/foodgram/foodgram/settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -144,7 +144,7 @@ STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_URL = 'media/'
 
-MEDIA_ROOT = BASE_DIR / MEDIA_URL
+MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
