0d09ccd36b3c97c27a62deff033d6def1f1983ee
ngrachik <ngrachik2011@gmail.com>
2023-06-09 12:14:56 +0300

    Changes to be committed: 	modified:   fixtures.py 	modified:   new_ingredients.json

--- data/fixtures.py
+++ data/fixtures.py
@@ -3,10 +3,12 @@ import json
 with open('ingredients.json', 'r') as f:
     data = json.load(f)
 
-for i, obj in enumerate(data):
-    obj.update({"model": "recipes.Ingredient", "pk": i+1})
+for i in range(len(data)):
+    data[i]["pk"] = i+1
+    data[i]["model"] = "recipes.Ingredient"
+    data[i] = {"model": data[i]["model"],
+               "pk": data[i]["pk"],
+               "fields": {"name": data[i]["name"], "measurement_unit": data[i]["measurement_unit"]}}
 
-updated_data = json.dumps(data, ensure_ascii=False)
-print(updated_data)
-with open('new_ingredients.json', 'w', encoding='Windows-1251') as f:
-    f.write(updated_data)
+with open("new_ingredients.json", "w", encoding="Windows-1251") as f:
+    f.write(json.dumps(data, ensure_ascii=False))
