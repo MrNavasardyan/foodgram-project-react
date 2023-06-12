fb6badcd50dba0452f8721539335664009510347
ngrachik <ngrachik2011@gmail.com>
2023-06-12 15:49:32 +0300

    Changes to be committed: 	modified:   backend/foodgram/foodgram/settings.py

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -19,7 +19,6 @@ CSRF_TRUSTED_ORIGINS = [
     'http://*.localhost',
     'http://127.0.0.1:8080'
     'http://localhost:8080',
-    '158.160.65.32',
     'http://foodgramng.ddns.net',
     'https://foodgramng.ddns.net',
     'http://158.160.65.32',
